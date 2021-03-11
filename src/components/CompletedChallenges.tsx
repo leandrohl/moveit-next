eac50adfcce34981b0ba5983c1bac7d66321e7e4
Leandro Henrique <leandro.henrique@unesp.br>
2021-03-11 14:52:57 -0300

    implementando componentes de desafios completos

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges(){
+    return(
+        <div className={styles.completedChallengesContainer}>
+            <span>Desafio completos</span>
+            <span>5</span>
+        </div>
+    );
+}
