2d76ae58e767f74736e432ebb6944252f78c60b4
Leandro Henrique <leandro.henrique@unesp.br>
2021-03-11 17:07:32 -0300

    implementando o componente challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Head from 'next/head'
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head'
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -14,8 +15,9 @@ export default function Home() {
       <Head>
         <title>Início | move.it</title>
       </Head>
-
+      
       <ExperienceBar />
+      
       
       <section>
         <div>
@@ -24,7 +26,7 @@ export default function Home() {
           <Countdown/>
         </div>
         <div>
-
+          <ChallengeBox/>
         </div>
       </section>
     </div>
